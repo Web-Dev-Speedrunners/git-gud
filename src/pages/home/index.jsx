3552c33d4515d6fd6af1ec27e8958c7c0fd3d096
muskankapoor <muskankapoor016@gmail.com>
2021-02-12 19:57:08 -0500

    value of trees

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import Banner from './banner'
+import Section from './section'
 
 const HomePage = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <Banner />
-      <h4> The Value of Trees</h4>
-      <ul>
-        <li>For the purification of air</li>
-        <li>Produce oxygen</li>
-        <li>Store Carbon</li>
-        <li>Are homes for many birds and animals</li>
-        <li>Vital in energy conservation</li>
-      </ul>
+      <Section/>
   </div>
-
   )
 
 }
---
+++ src/pages/home/section.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./section.css";
+
+
+const Section = () => {
+    return( 
+    <div id="sec-text">
+      <h4> Trees are vital</h4>
+      <ul>
+          <li>for the purification of air</li>
+          <li>to produce oxygen</li>
+          <li>to store carbon</li>
+          <li>as homes for many birds and animals</li>
+          <li>for energy conservation</li>
+      </ul>
+  </div>
+    )
+}
+
+  
+
+export default Section;
