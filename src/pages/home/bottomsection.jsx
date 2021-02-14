121c3c444d792e765f14feafa3c34867f4102458
muskankapoor <muskankapoor016@gmail.com>
2021-02-14 13:19:42 -0500

    bottom section

---
+++ src/pages/home/bottomsection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./section.css";
+
+const BottomSection = () => {
+    return( 
+    <div>
+        <h4>World without trees </h4>
+        <ul>
+            <li>No Trees... No Humans.</li>
+            <li>Higher amounts of carbon</li>
+            <li>Filthy air</li>
+            <li>Destruction of many organisms</li>
+            <li>Climate change and enviornment destruction</li>
+        </ul>
+    </div>
+     
+    )
+}
+
+  
+
+export default BottomSection;
+
+
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from './banner'
 import RightSection from './rightsection'
 import Section from './section'
+import BottomSection from './bottomsection'
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Banner />
       <Section/>
       <RightSection/>
+      <BottomSection/>
   </div>
   )
 
