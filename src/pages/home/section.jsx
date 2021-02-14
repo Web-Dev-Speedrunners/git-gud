ec0c1f25773a196e0e3a10fa3a3f802ec9e32773
muskankapoor <muskankapoor016@gmail.com>
2021-02-14 12:59:51 -0500

    updating facts

--- src/pages/home/section.jsx
+++ src/pages/home/section.jsx
@@ -14,13 +14,13 @@ const Section = () => {
           <li>for energy conservation</li>
       </ul>
 
-      <h4>World without Trees </h4>
+      <h4>World without trees </h4>
          <ul>
-          <li>for the purification of air</li>
-          <li>to produce oxygen</li>
-          <li>to store carbon</li>
-          <li>as homes for many birds and animals</li>
-          <li>for energy conservation</li>
+          <li>No Trees... No Humans.</li>
+          <li>Higher amounts of carbon</li>
+          <li>Filthy air</li>
+          <li>Destruction of many organisms</li>
+          <li>Climate change and enviornment destruction</li>
       </ul>
   </div>
     )
