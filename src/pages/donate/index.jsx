b685b0c53e164c986cc4db810a777bd7f1211d66
Xuejin <Xuejingao@gmail.com>
2021-02-14 13:24:13 -0500

    MVPfor Donation Page

--- src/pages/donate/index.jsx
+++ src/pages/donate/index.jsx
@@ -1,12 +1,59 @@
 import React from 'react'
 import DonateForm from './form'
+import SupportInfo from './supportInfo'
 
+import { makeStyles } from '@material-ui/styles'
+import { THEME_COLOR } from '../../constants/theme'
+import {
+  Container,
+  Row,
+  Col,
+	Card
+} from 'react-bootstrap'
+
+const styles = makeStyles({
+	pageHeader: {
+		backgroundColor: THEME_COLOR.Secondary,
+		color: THEME_COLOR.Primary,
+		marginTop: "24px",
+	},
+	titles: {
+		color: THEME_COLOR.Secondary,
+	}
+})
 
 const DonatePage = () => {
+	const style_classes = styles()
+
   return (
     <>
-    <h1>Donate Page</h1>
-    <DonateForm />
+		{/* TODO: Add function that will get the current donation amount */}
+		<Container className="text-center">
+			<h1 className={style_classes.pageHeader}>Help Save Trees Today</h1>
+			<p>Total Donation</p>
+			<p>$Donation$</p>
+		</Container>
+		
+		<Container className="mt-4">
+			<Row>
+				<Col>
+					{/* <h3>Donate</h3> */}
+					{/* <DonateForm /> */}
+					<Card>
+						<Card.Header>
+							<h3 className={style_classes.titles}>Donate</h3>
+						</Card.Header>
+						<Card.Body>
+							<DonateForm />
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col>
+					<h3 className={style_classes.titles}>Broke? Get Involved!</h3>
+					<SupportInfo />
+				</Col>
+			</Row>
+		</Container>
     </>
   )
 }
