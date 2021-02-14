54a4ed70982ef82e955da1766da9d07fb1f5fff6
Asif Shikder <shikdernyc@gmail.com>
2021-02-13 22:18:03 -0500

    Added Theme Color variables

---
+++ src/constants/theme.js
@@ -0,0 +1,9 @@
+export const THEME_COLOR = {
+  Primary: "#23260E",
+  Secondary: "#818C2E",
+  Tertiary: "#515914",
+  Important: "#A62F24",
+  Light: "#F2C1AE",
+  White: "#FFFFFF",
+  Black: "#000000"
+}
