fe44dc6b018d681d51bab3fd1cda585e00928240
Blake Vente <venteralph@gmail.com>
2021-02-15 21:52:08 -0500

    feat: responsive navbar with default background color

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from 'reactstrap'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Navbar = () => (
-  <div className="text-center">
-    <div>
-      Pretend this is navbar
-    </div>
-    <Link to="/"><Button>Home</Button></Link>
-    <Link to="/facts"><Button>Facts</Button></Link>
-    <Link to="/donate"><Button>Donate</Button></Link>
-  </div>
-)
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+} from "reactstrap";
 
-export default Navbar
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/facts", text: "Facts" },
+  { href: "/donate", text: "Donate" },
+]
+
+const CreateNavItem = ({ href, text }) => (
+  <NavItem>
+    <Link to={href} className="nav-link">
+      {text}{" "}
+    </Link>
+  </NavItem>
+);
+
+const NavBar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <>
+      <Navbar color="dark" className="p-3" dark expand="md">
+        <NavbarBrand href="/">Save The Trees</NavbarBrand>
+        <NavbarToggler onClick={() => setCollapsed(!collapsed)} />
+        <Collapse isOpen={collapsed} navbar>
+          <Nav navbar>
+            {links.map((item, i) => (
+              <CreateNavItem {...item} key={i} />
+            ))}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar
